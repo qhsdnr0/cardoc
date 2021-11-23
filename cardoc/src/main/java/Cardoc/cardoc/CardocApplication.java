ddb2987d85ec6d0b9e1d91169b18dd33733ee734
qhsdnr0 <qhsdnr0@gmail.com>
2021-11-24 08:33:16 +0900

    Initial commit

---
+++ cardoc/src/main/java/Cardoc/cardoc/CardocApplication.java
@@ -0,0 +1,13 @@
+package Cardoc.cardoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardocApplication.class, args);
+	}
+
+}
---
+++ cardoc/src/test/java/Cardoc/cardoc/CardocApplicationTests.java
@@ -0,0 +1,13 @@
+package Cardoc.cardoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
