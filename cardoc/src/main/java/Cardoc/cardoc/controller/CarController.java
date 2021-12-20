8ad26f22620c78fa9554e09eed1e52b47d3b9f37
qhsdnr0 <qhsdnr0@gmail.com>
2021-12-20 16:28:14 +0900

    Modify : CrossOrigin 어노테이션 추가, README 추가
    - cors 설정을 위한 CrossOrigin 어노테이션 추가
    
    - README.md 파일 추가

--- cardoc/src/main/java/Cardoc/cardoc/controller/CarController.java
+++ cardoc/src/main/java/Cardoc/cardoc/controller/CarController.java
@@ -5,15 +5,13 @@ import Cardoc.cardoc.repository.CarRepository;
 import Cardoc.cardoc.service.CarService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Transactional
 @RequestMapping("cars")
 @RestController
+@CrossOrigin
 public class CarController {
 
     private final CarService carService;
--- cardoc/src/main/java/Cardoc/cardoc/controller/UserController.java
+++ cardoc/src/main/java/Cardoc/cardoc/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("users")
 @Transactional
+@CrossOrigin
 public class UserController {
 
     private final UserService userService;
