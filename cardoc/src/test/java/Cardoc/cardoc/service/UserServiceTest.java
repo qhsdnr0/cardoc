b89efde1c47fd4411b84c0457304ba04e1d20b9b
qhsdnr0 <qhsdnr0@gmail.com>
2022-01-11 17:26:40 +0900

    Create : UserServiceTest
    - UserService 각 메소드 테스트 추가

--- cardoc/src/test/java/Cardoc/cardoc/service/UserServiceTest.java
+++ cardoc/src/test/java/Cardoc/cardoc/service/UserServiceTest.java
@@ -3,7 +3,13 @@ package Cardoc.cardoc.service;
 import Cardoc.cardoc.controller.UserForm;
 import Cardoc.cardoc.models.User;
 import Cardoc.cardoc.repository.UserRepository;
+import Cardoc.cardoc.util.Token;
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -13,6 +19,7 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -23,17 +30,25 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class UserServiceTest {
 
-    @Autowired UserService userService;
-    @Autowired UserRepository userRepository;
-    @Autowired EntityManager em;
+    @Autowired private UserService userService;
+    @Autowired private UserRepository userRepository;
+    @Autowired private EntityManager em;
 
-//    @BeforeAll
-//    static void before() {
-//        MockitoAnnotations.openMocks(UserService.class);
+    @BeforeEach
+    void init() {
+        UserForm userForm = new UserForm();
+        userForm.setAccount("qhsdnr0");
+        userForm.setPassword("asdf1234!");
+        userService.createUser(userForm);
+    }
+
+//    @AfterEach
+//    void tearDown() {
+//        userRepository.removeUser();
 //    }
 
     @Test
-    public void createUser() {
+    void createUser() {
         //given
         UserForm userForm = new UserForm();
         userForm.setAccount("name11");
@@ -41,23 +56,32 @@ class UserServiceTest {
         assertNull(userRepository.findByAccount(userForm.getAccount()));
 
         userService.createUser(userForm);
-        em.flush();
         assertEquals(userRepository.findByAccount(userForm.getAccount()).getAccount(), userForm.getAccount());
     }
 
-//    @Test
-//    void loginUser() {
-//    }
+    @Test
+    void loginUser() {
+        UserForm userForm = new UserForm();
+        userForm.setAccount("qhsdnr0");
+        userForm.setPassword("asdf1234!");
+
+        assertEquals(Token.makeJwtToken(userRepository.findByAccount(userForm.getAccount())), userService.loginUser(userForm));
+    }
 //
-//    @Test
-//    void getUser() {
-//    }
-//
-//    @Test
-//    void testGetUser() {
-//    }
-//
-//    @Test
-//    void deleteUser() {
-//    }
+    @Test
+    void getUser() {
+        Long id = 1L;
+        User user = userRepository.findOne(id);
+        assertEquals(user, userService.getUser(id));
+    }
+
+
+    @Test
+    void deleteUser() {
+        Long id = 1L;
+        User user = userRepository.findOne(id);
+        userService.deleteUser(user);
+        assertNull(userRepository.findOne(1L));
+
+    }
 }
